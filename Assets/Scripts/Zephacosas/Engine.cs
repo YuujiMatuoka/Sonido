38c0e050d475321576460c9d13a66b27262c2f02
Zephaltor <alexyack36@gmail.com>
2025-12-11 17:38:38 -0300

    Engine funcional

---
+++ Assets/Scripts/Zephacosas/Engine.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engine : MonoBehaviour
+{
+    public AK.Wwise.RTPC weatherRTPC;
+
+    public int startValue = 0;
+
+    public int minValue = 0;
+    public int maxValue = 100;
+
+    private int targetValue;
+    private float currentValue; 
+
+    private bool playerInside = false;
+
+    void Start()
+    {
+        currentValue = startValue;
+        targetValue = startValue;
+
+        // Seteamos el valor inicial en Wwise
+        if (weatherRTPC != null)
+            weatherRTPC.SetGlobalValue(currentValue);
+    }
+
+    void Update()
+    {
+        if (playerInside)
+        {
+            //Debug.Log("Hola");
+            if (currentValue < maxValue)
+            {
+                currentValue += Time.deltaTime * 2;
+            }
+            else
+            {
+                currentValue = maxValue;
+            }
+            Debug.Log(currentValue);
+            if (weatherRTPC != null)
+                    weatherRTPC.SetGlobalValue(currentValue);
+            
+        }
+    }
+
+   void OnTriggerEnter(Collider other)
+   {
+        if (other.CompareTag("Player"))
+        {
+            playerInside = true;
+        }
+   }
+}
---
+++ Assets/Scripts/Zephacosas/Fan.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] float fanDistanceSound;
+    //FirstPersonController player; = FindObjectOfType<FirstPersonController>();
+    //Transform playerTransform = player.transform;
+    //Transform playerTransform;
+    //GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+    public AK.Wwise.RTPC weatherRTPC;
+
+    public int startValue = 0;
+
+    public int minValue = 0;
+    public int maxValue = 100;
+
+    private int targetValue;
+    private float currentValue; 
+
+    Vector3 direction;
+
+void Start()
+    {
+        //FirstPersonController player = FindObjectOfType<FirstPersonController>();
+        //playerTransform = player.transform;
+        currentValue = startValue;
+        targetValue = startValue;
+
+        if (weatherRTPC != null)
+            weatherRTPC.SetGlobalValue(currentValue);
+    }
+
+    void Update()
+    {
+        direction = player.position - transform.position;
+
+        if (direction.magnitude < fanDistanceSound)
+        {
+            //currentValue = direction.magnitude * 10;
+            //Debug.Log(direction.magnitude);
+            currentValue = (10 - direction.magnitude) * 10;
+        }
+        else if (direction.magnitude >= fanDistanceSound)
+        {
+            currentValue = 0;
+        }
+        Debug.Log(currentValue);
+        if (weatherRTPC != null)
+                    weatherRTPC.SetGlobalValue(currentValue);
+
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, fanDistanceSound);
+    }
+}
